c23cf8a940f85c6832741b409fb968e06e95c6e2
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 03:26:17 +0500

    Update AppContext.jsx

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -19,3 +19,21 @@ const initialState = {
 
     ]
 }
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    return (<AppContext.Provider value={{
+        budget: state.budget,
+        expenses: state.expenses,
+        dispatch,
+    }}
+    >
+        {props.children}
+    </AppContext.Provider>
+
+    )
+}
+
