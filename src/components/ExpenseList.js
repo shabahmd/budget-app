af36bfa7f2842ee6bf67aa24a52c58bbd0f6fef5
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 03:30:21 +0500

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -9,3 +9,21 @@ const Expenses = () => {
         { id: 2323412, name: "Fuel", cost: 300 },
         { id: 2334412, name: "Child Care", cost: 950 },
     ];
+
+    return (
+        <ul className='list-group'>
+            {expenses.map((expense) => {
+                return (
+                    <ExpenseItem
+                        key={expense.id} // Don't forget to add a key prop when mapping over components
+                        id={expense.id}
+                        name={expense.name}
+                        cost={expense.cost}
+                    />
+                );
+            })}
+        </ul>
+    );
+};
+
+export default Expenses;
