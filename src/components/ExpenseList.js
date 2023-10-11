a2fa40b51ed869f68a167f06c2c6540d34573cc2
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 03:30:12 +0500

    Update ExpenseList.js

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = () => {
+    const expenses = [
+        { id: 5413102, name: "Shopping", cost: 50 },
+        { id: 2354312, name: "Holiday", cost: 530 },
+        { id: 2176312, name: "Transportation", cost: 460 },
+        { id: 2323412, name: "Fuel", cost: 300 },
+        { id: 2334412, name: "Child Care", cost: 950 },
+    ];
