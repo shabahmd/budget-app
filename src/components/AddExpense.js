8c0373b70e9a542b8a856ac65c64214c76f347f1
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 14:55:47 +0500

    Update AddExpense.js

---
+++ src/components/AddExpense.js
@@ -0,0 +1,25 @@
+
+const AddExpenseForm = () => {
+
+
+    return (
+        <form >
+            <div className='row'>
+                <div className='col-sm col-lg-4'>
+                    <label form='name'>Name</label>
+                    <input
+                        required='required'
+                        type='text'
+                        className='form-control'
+                        id='name'
+                    />
+                </div>
+                <div className='col-sm col-lg-4'>
+                    <label form='cost'>Name</label>
+                    <input
+                        required='required'
+                        type='text'
+                        className='form-control'
+                        id='cost'
+                    />
+                </div>
