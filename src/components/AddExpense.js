bc64b3f999fbb95265b2e1c27b646a78501c0018
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 21:09:46 +0500

    Update AddExpense.js

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,29 +1,53 @@
+import React, { useState } from "react";
+import { AppContext } from "../AppContext";
+import { useContext } from "react";
+
+import { v4 as uuidv4 } from 'uuid';
 
 const AddExpenseForm = () => {
+    const { dispatch } = useContext(AppContext);
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
 
+    const onSubmit = (event) => {
+        event.preventDefault(); // Fix the typo here
+        const expense = {
+            id: uuidv4(),
+            name: name,
+            cost: parseInt(cost),
+        };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense,
+        });
+    }
 
     return (
-        <form >
+        <form onSubmit={onSubmit}>
             <div className='row'>
                 <div className='col-sm col-lg-4'>
-                    <label form='name'>Name</label>
+                    <label htmlFor='name'>Name</label>
                     <input
                         required='required'
                         type='text'
                         className='form-control'
                         id='name'
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
                     />
                 </div>
                 <div className='col-sm col-lg-4'>
-                    <label form='cost'>Name</label>
+                    <label htmlFor='cost'>Cost</label>
                     <input
                         required='required'
-                        type='text'
+                        type='number' // Change type to 'number'
                         className='form-control'
                         id='cost'
+                        value={cost}
+                        onChange={(event) => setCost(event.target.value)}
                     />
                 </div>
-                </div>
+            </div>
             <div className='row mt-3'>
                 <div className='col-sm'>
                     <button type='submit' className='btn btn-primary'>
