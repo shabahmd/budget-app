2bd89bbd8a91741d47cdc21842054c3c069b670f
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 21:11:14 +0500

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,21 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import ExpenseItem from "./ExpenseItem";
 
+import { AppContext } from "../AppContext";
 const Expenses = () => {
-    const expenses = [
-        { id: 5413102, name: "Shopping", cost: 50 },
-        { id: 2354312, name: "Holiday", cost: 530 },
-        { id: 2176312, name: "Transportation", cost: 460 },
-        { id: 2323412, name: "Fuel", cost: 300 },
-        { id: 2334412, name: "Child Care", cost: 950 },
-    ];
+    const { expenses } = useContext(AppContext)
 
     return (
-        <ul className='list-group'>
+        <ul>
             {expenses.map((expense) => {
                 return (
                     <ExpenseItem
-                        key={expense.id} // Don't forget to add a key prop when mapping over components
+                        key={expense.id}
                         id={expense.id}
                         name={expense.name}
                         cost={expense.cost}
