8cacd0f06a4da294e2f438b9f84b185b0606f5ff
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 21:12:42 +0500

    Update AppContext.jsx

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,39 +1,56 @@
 import { useReducer, createContext } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 
 const AppReducer = (state, action) => {
     switch (action.type) {
+        case "ADD_EXPENSE":
+            return {
+                ...state,
+                expenses: [...state.expenses, action.payload],
+            };
+
+        case 'DELETE_EXPENSE':
+            return {
+                ...state,
+                expenses: state.expenses.filter(
+                    (expense) => expense.id !== action.payload
+                ),
+            };
         default:
+
             return state;
     }
 
-}
+};
 
 
 const initialState = {
     budget: 2000,
     expenses: [
-        { id: 12, name: 'shopping', cost: 40 },
-        { id: 13, name: 'holiday', cost: 450 },
-
-
-    ]
-}
+        { id: uuidv4(), name: 'Shopping', cost: 50 },
+        { id: uuidv4(), name: 'Holiday', cost: 300 },
+        { id: uuidv4(), name: 'Transportation', cost: 70 },
+        { id: uuidv4(), name: 'Fuel', cost: 40 },
+        { id: uuidv4(), name: 'Child Care', cost: 500 },
+    ],
+};
 
 export const AppContext = createContext();
-
 export const AppProvider = (props) => {
+
     const [state, dispatch] = useReducer(AppReducer, initialState)
 
-    return (<AppContext.Provider value={{
-        budget: state.budget,
-        expenses: state.expenses,
-        dispatch,
-    }}
-    >
-        {props.children}
-    </AppContext.Provider>
+    return (
+        <AppContext.Provider
+            value={{
+                budget: state.budget,
+                expenses: state.expenses,
+                dispatch,
+            }}
+        >
+            {props.children}
+        </AppContext.Provider>
 
     )
 }
-
