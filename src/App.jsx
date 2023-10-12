41fbb298da2dc05cc21ea2792fc50b79b797e4dd
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 21:13:29 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,4 +23,22 @@ const App = () => {
           <div className='col-sm'>
             <ExpenseTotal />
           </div>
+        </div>        
+        <h3 className='mt-3'>Expenses</h3>
+        <div className='row '>
+          <div className='col-sm'>
+            <ExpenseList />
+          </div>
         </div>
+        <h3 className='mt-3'>Add Expense</h3>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <AddExpenseForm />
+          </div>
+        </div>
+      </div>
+    </AppProvider>
+  );
+};
+
+export default App;
