cf1d1d7270c94159a9ad39c3a0e3f3bb1a1ed70f
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 15:04:18 +0500

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
 const ExpenseTotal = () => {
+
+    return (
+        <div className='alert alert-primary p-4'>
+            <span>Spent so far: $i770 </span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
