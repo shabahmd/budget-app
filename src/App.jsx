c1aac1233580c2e70725e535b565a1a9bfbeb47e
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 21:13:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Budget from './components/Budget.jsx'
-import Remaining from './components/Remaining.jsx'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { AppProvider } from './AppContext.jsx';
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AddExpenseForm from './components/AddExpenseForm';
+import RemainingBudget from './components/Remaining';
 
 const App = () => {
-
   return (
-
-
-    <div className="container">
-      <h1 className="mt-3"> My Budget Planner</h1>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <Budget />
+    <AppProvider>
+      <div className='container'>
+        <h1 className='mt-3'>My Budget Planner</h1>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <Budget />
+          </div>
+          <div className='col-sm'>
+            <RemainingBudget />
+          </div>
+          <div className='col-sm'>
+            <ExpenseTotal />
+          </div>
         </div>
-        <div className="col-sm">
-          <Remaining />
-        </div>
-
-      </div>
-    </div>
-  );
-
-};
-
-export default App;
