827d05f8ca54758da778f2de1515aaa307bf92b9
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-12 21:11:36 +0500

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AppContext } from "../AppContext";
 
 const ExpenseTotal = () => {
+    const { expenses } = useContext(AppContext);
+
+    // Calculate the total cost from the expenses array
+    const totalCost = expenses.reduce((total, expense) => total + expense.cost, 0);
 
     return (
         <div className='alert alert-primary p-4'>
-            <span>Spent so far: $i770 </span>
+            <span>Spent so far: ${totalCost}</span>
         </div>
     );
 };
